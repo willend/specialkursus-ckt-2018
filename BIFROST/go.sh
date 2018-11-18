5b443cccf2662698e60a2b617fab9f5b38da4ee4
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2018-11-18 21:18:25 +0100

    New set of scans

--- BIFROST/go.sh
+++ BIFROST/go.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# V-scan, 181 points over 90 degrees
-mcrun --mpi=16 Full_simple.instr INCOH=1 -n1e7  Omega=0,90 -N181 -dV_scan
-# Magnon-scan, 181 points over 90 degrees
-mcrun --mpi=16 Full_simple.instr INCOH=0 -n1e8  Omega=0,90 -N181 -dMagnon_scan
+# V-run
+mcrun --mpi=16 Full_simple.instr INCOH=1 -n1e8  Omega=0 -dVrun
+mcrun --mpi=16 Full_simple.instr INCOH=1 INCOHspread=1 -n1e8 -dVrun_inel
+# Magnon-scan, 46 points over 90 degrees
+mcrun --mpi=16 Full_simple.instr INCOH=0 -n1e8  Omega=0,90 -N146 -dMagnon_rerun
