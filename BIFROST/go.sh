d2d8726af995d3f49ac064e8e6c09081acef02bc
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2018-11-18 21:19:58 +0100

    shorter

--- BIFROST/go.sh
+++ BIFROST/go.sh
@@ -3,4 +3,4 @@
 mcrun --mpi=16 Full_simple.instr INCOH=1 -n1e8  Omega=0 -dVrun
 mcrun --mpi=16 Full_simple.instr INCOH=1 INCOHspread=1 -n1e8 -dVrun_inel
 # Magnon-scan, 46 points over 90 degrees
-mcrun --mpi=16 Full_simple.instr INCOH=0 -n1e8  Omega=0,90 -N146 -dMagnon_rerun
+mcrun --mpi=16 Full_simple.instr INCOH=0 -n2e7  Omega=0,90 -N46 -dMagnon_rerun
