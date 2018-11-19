95efb604512ed02146c4e1d6b77c2d923f10aff6
Peter Wwillendrup <pkwi@fysik.dtu.dk>
2018-11-19 16:33:20 +0100

    Another rerun..

--- BIFROST/go.sh
+++ BIFROST/go.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # V-run
-mcrun --mpi=16 Full_simple.instr INCOH=1 -n1e8  Omega=0 -dVrun
-mcrun --mpi=16 Full_simple.instr INCOH=1 INCOHspread=1 -n1e8 -dVrun_inel
+#mcrun --mpi=16 Full_simple.instr INCOH=1 -n1e8  Omega=0 -dVrun
+#mcrun --mpi=16 Full_simple.instr INCOH=1 INCOHspread=1 -n1e8 -dVrun_inel
 # Magnon-scan, 46 points over 90 degrees
-mcrun --mpi=16 Full_simple.instr INCOH=0 -n2e7  Omega=0,90 -N46 -dMagnon_rerun
+mcrun --mpi=16 Full_simple.instr INCOH=0 -n1e8  Omega=0,90 -N46 TwoTheta=10 -dMagnon_rerun_events_A
+mcrun --mpi=16 Full_simple.instr INCOH=0 -n1e8  Omega=0,90 -N46 TwoTheta=20 -dMagnon_rerun_events_B
