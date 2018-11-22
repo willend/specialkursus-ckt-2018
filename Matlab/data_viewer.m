39019bfb7e0a3661bfdabc2f98a8c001a8be9471
CryoGit <42933385+CryoGit@users.noreply.github.com>
2018-11-22 10:59:03 +0100

    ToF results from analyser crystals in Matlab

--- Matlab/data_viewer.m
+++ Matlab/data_viewer.m
@@ -2,13 +2,16 @@
 close all
 clear all
 
-addpath(genpath('/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Matlab'))       %replace with own path 
-path = '/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Krystal_sim/Analy_crystal_20181105_102130';      %path of simulation
+%addpath(genpath('/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Matlab'))       %replace with own path 
+addpath(genpath('C:\Users\kedde\Documents\GitHub\specialkursus-ckt-2018\Matlab'))
+%path = '/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Krystal_sim/Analy_crystal_20181105_102130';      %path of simulation
+path = 'C:\Users\kedde\Documents\40';      %path of simulation
+
 cd(path)
-d=dir('*.dat');
+d=dir('analyser*_tmon.t_y');
 N=size(d);
 
-Data = iData(d(1).name);
+Data = iData(d(2).name);
 plot(Data)
 
 
