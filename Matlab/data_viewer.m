ae94746f00412f637ad40df10ba6745bfee31aa1
CryoGit <42933385+CryoGit@users.noreply.github.com>
2018-11-26 09:57:07 +0100

    Updatet matlab code for data analysis of ToF data

--- Matlab/data_viewer.m
+++ Matlab/data_viewer.m
@@ -2,18 +2,24 @@
 close all
 clear all
 
-addpath(genpath('/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Matlab'))       %replace with own path 
-%addpath(genpath('C:\Users\kedde\Documents\GitHub\specialkursus-ckt-2018\Matlab'))
-path = '/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Krystal_sim/Analy_crystal_20181105_102130';      %path of simulation
-%path = 'C:\Users\kedde\Documents\40';      %path of simulation
+%addpath(genpath('/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Matlab'))       %replace with own path 
+addpath(genpath('C:\Users\kedde\Documents\GitHub\specialkursus-ckt-2018\Matlab'))
+%path = '/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Krystal_sim/Analy_crystal_20181105_102130';      %path of simulation
+path = 'C:\Users\kedde\Documents\Magnon_rerun_events_B\40';      %path of simulation
 
 cd(path)
-d=dir('*.dat');
+d=dir('*.t_y');
 N=size(d);
 
-Data = iData(d(2).name);
-plot(Data)
+Data = iData(d(1).name);
 
+y = Data.x;
+tof = Data.y;
+
+plot(Data)
+view(0,90)
+xlim([tof(1) tof(end)])
+ylim([y(1) y(end)])
 
 
 
--- Matlab/data_viewer2.m
+++ Matlab/data_viewer2.m
@@ -2,79 +2,105 @@
 close all
 clear all
 
-%addpath(genpath('/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Matlab'))       %replace with own path 
+%addpath(genpath('/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Matlab'))       %replace with own path
 addpath(genpath('C:\Users\kedde\Documents\GitHub\specialkursus-ckt-2018\Matlab'))
 %path = '/Users/TummasN/Documents/GitHub/specialkursus-ckt-2018/Krystal_sim/Analy_crystal_20181105_102130';      %path of simulation
-path = 'C:\Users\kedde\Documents\Magnon_rerun_events_B';      %path of simulation
+path = 'C:\Users\kedde\Documents\Magnon_rerun_events_B\';      %path of simulation
 
-cd(path)
-d = dir(fullfile(path,'**\analyser1_tmon.t_y'));
+Qmatrix = zeros(46,30,256);
+dEmatrix = zeros(46,30,256);
 
-for idx = 1:45
-    currentFile = fullfile(d(idx).folder);
+for nn = 0:45
+    cd(path)
+    cd(num2str(nn))
+    %d = dir(fullfile(path,'**\analyser1_tmon.t_y'));
+    
+    %for idx = 1:45
+    %    currentFile = fullfile(d(idx).folder);
+    %end
+    d=dir('**\analyser1_tmon.t_y');
+    
+    N=size(d);
+    
+    Data = iData(d.name);
+    
+    y = Data.x;
+    tof = Data.y;
+    I = Data.I;
+    %I(I==0)=NaN;
+%     
+%     if max(I)==0
+%         Qmatrix(nn+1,:) = Q;
+%         dEmatrix(nn+1,:) = DeltaE;
+%         continue
+%     end
+%     
+%     Tmax = zeros(1,30);
+%     for ii=1:30
+%         if max(I(ii,:)) == 0
+%             continue
+%         end
+%         i_max= find(I(ii,:) == max(I(ii,:)));
+%         Tmax(ii)=tof(i_max);
+%     end
+%     
+    
+    
+    %5 meV short dimensions in SI
+    Ef = 5e-3*1.602e-19; %
+    mn = 1.627e-27; %kg
+    hbar = 1.0546e-34; %J*s
+    
+    kf = sqrt(2*mn*Ef/hbar^2);
+    
+    Lsa = 1.544; %distance from sample to analyser
+    Lad = 1.623; %distance from analyser to detector
+    
+    wa = 0.1744; %width of analyser
+    wd = 0.2976; %width of detector
+    wad = wd*Lsa/(Lsa + Lad);
+    
+    ya = y*wad/wd;
+    
+    %Scattering angle
+    alpha = atan(ya./Lsa);
+    
+    Lsaalpha = Lsa./cos(alpha);
+    Ladalpha = Lad./cos(alpha);
+    
+    %Total length from sample to detector
+    L2 =abs(Lsaalpha + Ladalpha);
+    %The arrival time from sample to detector
+    t2 = L2./sqrt(2*Ef/mn);
+    
+    %The initial arrival time from source to sample
+    t1 = tof-t2;
+    
+    %Incoming neutrons wavevector and energy
+    ki = mn*162./(hbar*t1);
+    Ei = hbar^2*ki.^2/(2*mn);
+    
+    DeltaE = (Ei - Ef)/(1.602e-19)*1e3;
+    
+    %The scattering vector size
+    Q = sqrt(ki.^2+kf^2-2.*ki*kf.*cos(alpha))./1e10;
+    
+    DeltaE(find(Tmax==0))=NaN;
+    Q(find(Tmax==0))=NaN;
+    
+    Qmatrix(nn+1,:) = Q;
+    dEmatrix(nn+1,:) = DeltaE;
+    
+ 
+%     figure(1)
+%     plot(Data)
+%     view(0,90)
+%     xlim([tof(1) tof(end)])
+%     ylim([y(1) y(end)])
+%     figure(2)
+%     plot(Q,DeltaE,'o-')
+    %xlim([0 1])
 end
-
-%d=dir('*.t_y');
-N=size(d);
-
-Data = iData(d.folder);
-
-y = Data.x;
-tof = Data.y;
-
-%5 meV short dimensions
-Ef = 5e-3*1.602e-19; %
-mn = 1.627e-27; %kg
-hbar = 1.0546e-34; %J*s
-
-kf = sqrt(2*mn*Ef/hbar^2);
-
-Lsa = 1.544;
-Lad = 1.623;
-
-wa = 0.2976;
-wd = 0.2976;
-wad = wd*Lsa/(Lsa + Lad);
-
-ya = y.*Lsa/(Lsa+Lad);
-
-if abs(ya)>= -wd/2 & abs(ya) <= wd/2
-    ya = ya;
-else
-    ya = [];
-end
-
-%Scattering angle
-alpha = atan(ya./Lsa)*180/pi();
-
-Lsaalpha = cos(alpha).*Lsa;
-Ladalpha = cos(alpha).*Lad;
-
-%Total length from sample to detector
-L2 =abs(Lsaalpha + Ladalpha);
-%The arrival time from sample to detector
-t2 = L2./sqrt(2*Ef/mn);
-
-%The initial arrival time from source to sample
-t1 = tof(1:30)-t2;
-
-%Incoming neutrons wavevector and energy
-ki = mn*162./(hbar.*t1);
-Ei = hbar^2.*ki.^2/(2*mn);
-
-DeltaE = (Ei - Ef)/(1.602e-19)*1e3;
-
-%The scattering vector size
-Q = sqrt(ki.^2+kf^2-2.*ki.*kf.*cos(alpha.*pi()/180))./1e10;
-
-figure(1)
-plot(Data)
-view(0,90)
-xlim([tof(1) tof(end)])
-ylim([y(1) y(end)])
-%figure(2)
-%plot(Q,DeltaE)
-
-
+plot(Qmatrix,dEmatrix,'o')
 
 
